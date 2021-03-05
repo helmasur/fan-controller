6d1ed85a344bb18d8aca541542dbca25538aa751
Martin Jacobson <helmasur.king@protonmail.com>
2021-03-05 09:33:09 +0100

    led debug functions

--- main.c
+++ main.c
@@ -43,60 +43,173 @@
     TERMS.
 */
 
+//#include <xc.h>
 #include "mcc_generated_files/mcc.h"
 #include <math.h>
 
 #define BTIME 100
 
+unsigned char ledstate = 0;
+unsigned char button_pressed = 0;
+
+void set_button_pressed(){
+    button_pressed = 1;
+}
+
+void reset_button_pressed(){
+    button_pressed = 0;
+}
+
+unsigned int calc_temp(unsigned int adc_val_int){
+    double adc_val = (double)adc_val_int;
+    adc_val -= 39; //kompensera för nollnivå som inte är noll
+    if (adc_val < 1.0) adc_val = 1.0;
+    double ntc_a = 0.003354016;
+    double ntc_b = 0.0002744032;
+    double resistor = 9850.0;
+    double ntc_resistance = resistor * ((984.0/adc_val) - 1.0); //984 efterson nollpunkten har offset
+    // resistans 24399 ok
+    double ratio = ntc_resistance / (22000.0); //22000 is NTC reference resistance at 25 celsius;
+    double log_ratio = log(ratio);
+    double temp = 1.0 / (ntc_a + (ntc_b * log_ratio) );
+    temp -= 273.15;
+    temp *= 10.0;
+    return (int)temp;
+}
+
 void leds_on(){
-    LED_4_SetHigh();
-    LED_5_SetHigh();
-    LED_6_SetHigh();
-    LED_7_SetHigh();
+    LED_D4_SetHigh();
+    LED_D5_SetHigh();
+    LED_D6_SetHigh();
+    LED_D7_SetHigh();
 }
 
 void leds_off(){
-    LED_4_SetLow();
-    LED_5_SetLow();
-    LED_6_SetLow();
-    LED_7_SetLow();
+    LED_D4_SetLow();
+    LED_D5_SetLow();
+    LED_D6_SetLow();
+    LED_D7_SetLow();
 }
 
-void blink(){
+void blink_0(){
+    LED_D7_SetHigh();
+    __delay_ms(BTIME);
+    LED_D7_SetLow();
+    __delay_ms(BTIME);
+}
+
+void set_leds(int value){
+    if (value & 0b1000000000000000) LED_D7_SetHigh();
+    else LED_D7_SetLow();
+    if (value & 0b0100000000000000) LED_D6_SetHigh();
+    else LED_D6_SetLow();
+    if (value & 0b0010000000000000) LED_D5_SetHigh();
+    else LED_D5_SetLow();
+    if (value & 0b0001000000000000) LED_D4_SetHigh();
+    else LED_D4_SetLow();
+}
+
+void blink_1(){
     leds_on();
     __delay_ms(BTIME);
     leds_off();
     __delay_ms(BTIME);
 }
 
-void set_leds(int value){
-    if (value & 0b1000000000000000) LED_7_SetHigh();
-    else LED_7_SetLow();
-    if (value & 0b0100000000000000) LED_6_SetHigh();
-    else LED_6_SetLow();
-    if (value & 0b0010000000000000) LED_5_SetHigh();
-    else LED_5_SetLow();
-    if (value & 0b0001000000000000) LED_4_SetHigh();
-    else LED_4_SetLow();
-}
-
 void blink_int(unsigned int value){
     leds_off();
+    blink_1();
+    __delay_ms(500);
     for(int pos=0; pos<4; pos++){
-        blink();
+        blink_1();
         set_leds(value);
         __delay_ms(1500);
         leds_off();
+        __delay_ms(150);
         value = value << 4;
     }
     __delay_ms(2000);
 }
+
+void blink_temp(void){
+    unsigned int current_value;
+    current_value = ADC_GetConversion(NTC);
+    current_value = current_value >> 6;
+    unsigned int temp = (unsigned int)calc_temp(current_value);
+    leds_off();
+    for(int pos=0; pos<4; pos++){
+        blink_1();
+        set_leds(temp);
+        __delay_ms(1500);
+        leds_off();
+        temp = temp << 4;
+    }
+    __delay_ms(2500);
+}
+
+void blink_current_int(void){
+    unsigned int current_value;
+    current_value = ADC_GetConversion(NTC);
+    current_value = current_value >> 6;
+//    current_value = 0b1111000011001010;
+    leds_off();
+    blink_1();
+    __delay_ms(500);
+    for(int pos=0; pos<4; pos++){
+        blink_1();
+        set_leds(current_value);
+        __delay_ms(1500);
+        leds_off();
+        __delay_ms(150);
+        current_value = current_value << 4;
+    }
+    __delay_ms(2000);
+}
+
+void seg_off(){
+//    IO_RC1_SetHigh();
+    IO_RC2_SetHigh();
+    IO_RB4_SetHigh();
+    IO_RB5_SetHigh();
+    IO_RB6_SetHigh();
+    IO_RC7_SetHigh();
+    IO_RC6_SetHigh();
+    IO_RC3_SetHigh();
+}
+
+void seg_nr(char num){
+    seg_off();
+    if (num==1){
+        IO_RC7_SetLow();
+        IO_RC6_SetLow();
+    }
+    else if (num==2){
+        
+    }
+}
+
+void switchleds(){
+        if (ledstate == 1){
+            leds_off();
+            ledstate = 0;
+        }
+        else{
+            leds_on();
+            ledstate = 1;
+        }
+    }
+
+
+
 
 /*
                          Main application
  */
 void main(void)
 {
+    int adcResult;
+    unsigned int temp = 0;
+    
     // initialize the device
     SYSTEM_Initialize();
 
@@ -104,24 +217,74 @@ void main(void)
     // Use the following macros to:
 
     // Enable the Global Interrupts
-    //INTERRUPT_GlobalInterruptEnable();
+    INTERRUPT_GlobalInterruptEnable();
 
     // Enable the Peripheral Interrupts
-    //INTERRUPT_PeripheralInterruptEnable();
+    INTERRUPT_PeripheralInterruptEnable();
 
     // Disable the Global Interrupts
     //INTERRUPT_GlobalInterruptDisable();
 
     // Disable the Peripheral Interrupts
     //INTERRUPT_PeripheralInterruptDisable();
+    
+//    TMR1_SetInterruptHandler(switchleds);
+    //Enable the TMR0 Interrupts 
+//    TMR1IE = 1;
+    INT_SetInterruptHandler(set_button_pressed);
+    EXT_INT_fallingEdgeSet();
+    
+    
 
+    
+    
+    
+//    662 689     659 704 706
+    
+    
+    IO_RA0_SetHigh();       //tänder led
+//    IO_RC1_SetLow();
+    IO_RC2_SetLow();
+    IO_RB4_SetLow();
+    IO_RB5_SetLow();
+    IO_RB6_SetLow();
+    IO_RC7_SetLow();
+    IO_RC6_SetLow();
+    IO_RC3_SetLow();
+    
+    
+    
     while (1)
     {
-        double qwer = log(1.14)*1000;
-        int asdf = (int)qwer;
-        blink_int(qwer);
+        if (button_pressed == 1){
+            blink_temp();
+            reset_button_pressed();
+        }
         
-        // Add your application code
+        
+//        __delay_ms(50);
+        
+//        __delay_ms(950);
+        
+        //Curiosity LEDs: 4,5,6,7   RA5     RA1     RA2     RC5
+        //          PIN:            2       18      17      5
+        
+//        Test minsta ADC värde: 39. Max 1023.
+        
+        
+        //Start ADC conversion
+        adcResult = ADC_GetConversion(NTC);
+        
+        
+        
+        adcResult = adcResult >> 6;
+//        adcResult -= 650;
+//        adcResult *= 17;
+        
+
+        //Make the adcResult the PWM duty cycle
+//        PWM3_LoadDutyValue(adcResult);
+        temp = calc_temp(adcResult);
     }
 }
 /**
